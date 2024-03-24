3ea93d2e592d98e662adb89daa67f39e0b964bc2
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-03-24 12:10:14 +0600

    added card sectio and some style added

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -13,7 +13,7 @@ const FeaturedJobs = () => {
 
 
       return (
-            <div>
+            <div className=''>
                   <div className='text-center'>
                         <h2 className="text-5xl">Featured Jobs { jobs.length}</h2>
                         <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique blanditiis est eos nam aperiam deleniti! Cupiditate, ducimus error doloribus deleniti placeat debitis quos et nesciunt expedita laudantium tenetur explicabo rerum.</p>
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Job = () => {
+const Job = ({ job }) => {
+      const { logo,job_title, company_name, remote_or_onsite, location, salary, job_type  } = job;
+      console.log(job);
       return (
-            <div>
-                  this is a job
+            <div className="card card-compact bg-base-100 shadow-xl">
+                  <figure><img src={logo} alt="Shoes" /></figure>
+                  <div className="card-body">
+                        <h2 className="card-title">{job_title}</h2>
+                        <p>{company_name}</p>
+                        <div>
+                              <button className='px-5 py-2 border font-extrabold rounded text-[#7E90FE] mr-4 border-[#7E90FE]'>{remote_or_onsite}</button>
+                              <button className='px-5 py-2 border font-extrabold rounded text-[#7E90FE] mr-4 border-[#7E90FE]'>{job_type}</button>
+                        </div>
+                        <div className="card-actions">
+                              <button className="btn btn-primary">View Details</button>
+                        </div>
+                  </div>
             </div>
       );
 };
